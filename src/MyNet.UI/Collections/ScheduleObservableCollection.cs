7358bf0313b2e8535dc218ae20981295fbd57b59
Stéphane ANDRE <andre.cs2i@gmail.com>
2024-06-14 00:09:11 +0200

    fix!: provide function to get Scheduler in ScheduleObservableCollection

--- src/MyNet.UI/Collections/ScheduleObservableCollection.cs
+++ src/MyNet.UI/Collections/ScheduleObservableCollection.cs
@@ -6,15 +6,16 @@ using System.Reactive.Concurrency;
 using DynamicData.Binding;
 using DynamicData;
 using MyNet.Utilities.Collections;
+using System;
 
 namespace MyNet.UI.Collections
 {
     public class ScheduleObservableCollection<T> : ThreadSafeObservableCollection<T>, IObservableCollection<T>, IExtendedList<T>
     {
-        public ScheduleObservableCollection(IScheduler scheduler) : base(x => scheduler.Schedule(x)) { }
+        public ScheduleObservableCollection(Func<IScheduler> scheduler) : base(x => scheduler().Schedule(x)) { }
 
-        public ScheduleObservableCollection(List<T> list, IScheduler scheduler) : base(list, x => scheduler.Schedule(x)) { }
+        public ScheduleObservableCollection(List<T> list, Func<IScheduler> scheduler) : base(list, x => scheduler().Schedule(x)) { }
 
-        public ScheduleObservableCollection(IEnumerable<T> collection, IScheduler scheduler) : base(collection, x => scheduler.Schedule(x)) { }
+        public ScheduleObservableCollection(IEnumerable<T> collection, Func<IScheduler> scheduler) : base(collection, x => scheduler().Schedule(x)) { }
     }
 }
