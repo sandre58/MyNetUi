89723d566ff30f21aab27281563f913c633821bc
Stéphane ANDRE (E104915) <stephane.andre_ext@michelin.com>
2024-09-20 12:00:41 +0200

    feat: add ItemDialogViewModel

--- src/MyNet.UI/ViewModels/Workspace/ItemViewModel.cs
+++ src/MyNet.UI/ViewModels/Workspace/ItemViewModel.cs
@@ -9,6 +9,7 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using MyNet.UI.Commands;
 using MyNet.UI.Dialogs;
+using MyNet.UI.Dialogs.Models;
 using MyNet.UI.Extensions;
 using MyNet.UI.Locators;
 using MyNet.UI.ViewModels.Edition;
@@ -66,6 +67,95 @@ namespace MyNet.UI.ViewModels.Workspace
         }
 
         #endregion Edit
+    }
+
+    public abstract class ItemDialogViewModel<TModel> : ItemViewModel<TModel>, IDialogViewModel
+        where TModel : INotifyPropertyChanged
+    {
+        #region Members
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Gets or sets the result.
+        /// </summary>
+        public bool? DialogResult { get; set; }
+
+        public virtual bool LoadWhenDialogOpening => true;
+
+        /// <summary>
+        /// Gets or sets close command.
+        /// </summary>
+        public ICommand CloseCommand { get; private set; }
+
+        #endregion Members
+
+        #region Constructors
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Initialise a new instance of <see cref="DialogViewModel" />.
+        /// </summary>
+        protected ItemDialogViewModel() => CloseCommand = CommandsManager.Create<bool?>(Close, CanClose);
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Can Close ?
+        /// </summary>
+        /// <param name="dialogResult"></param>
+        /// <returns></returns>
+        protected virtual bool CanClose(bool? dialogResult) => true;
+
+        /// <summary>
+        /// Closes the dialog.
+        /// </summary>
+        public virtual void Close(bool? dialogResult)
+        {
+            if (dialogResult is not null)
+                DialogResult = dialogResult.Value;
+
+            var e = new CancelEventArgs();
+            OnCloseRequest(e);
+
+            CloseRequest?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Closes the dialog.
+        /// </summary>
+        public virtual void Close() => Close(null);
+
+        /// <summary>
+        /// Closes the dialog.
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void OnCloseRequest(CancelEventArgs e)
+        {
+            // Method intentionally left empty.
+        }
+
+        public virtual Task<bool> CanCloseAsync() => Task.FromResult(true);
+
+        public virtual void Load()
+        {
+            DialogResult = null;
+
+            Refresh();
+        }
+
+        #endregion Methods
+
+        #region Events
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Closes the dialog.
+        /// </summary>
+        public event CancelEventHandler? CloseRequest;
+
+        #endregion Events
     }
 
     public abstract class ItemViewModel<TModel> : NavigableWorkspaceViewModel, IItemViewModel<TModel>
